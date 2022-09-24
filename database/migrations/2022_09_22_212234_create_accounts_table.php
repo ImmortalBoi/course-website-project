91e39c892077745bfd9bee3aac2679cdd5d386f2
Devikaze <83789517+Devikaze@users.noreply.github.com>
2022-09-24 03:47:38 +0200

    fixed most migration problems

---
+++ database/migrations/2022_09_22_212234_create_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts',function (Blueprint $table){
+            $table->id('account_id');
+            $table->string('account_email')->unique();
+            $table->string('account_password');
+            $table->enum('account_type',array('admin','instructor','student'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
---
+++ database/migrations/2022_09_22_221018_create_instructors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('instructors', function (Blueprint $table) {
+            $table->foreignId('instructor_id')->references('account_id')->on('accounts');
+            $table->string('instructor_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('instructors');
+    }
+};
