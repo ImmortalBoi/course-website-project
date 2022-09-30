c2c5dbaf508b348e0700416f5b56dfcfd54a8b87
Khaled Eldesoukey <54212167+ImmortalBoi@users.noreply.github.com>
2022-09-30 21:23:58 +0200

    Added pagination

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -45,43 +45,77 @@
             <div class="row">
 
                 @foreach ($courses as $course)
-                <?php $name = DB::select('select instructor_name from instructors where id ='.$course->instructor_id);?>
-                <div class="col-lg-4 col-md-6 pb-4">
-                    <a class="courses-list-item position-relative d-block overflow-hidden mb-2" href="{{ Route('course.show',$course->id ) }}">
-                        <img class="img-fluid" src="{{Storage::url($course->course_img)}}" onerror="this.src='{{ URL::to('/') }}/img/no-background.jpg';">
-                        <div class="courses-text">
-                            <h4 class="text-center text-white px-3">{{$course->course_name}}</h4>
-                            <div class="border-top w-100 mt-3">
-                                <div class="d-flex justify-content-between p-4">
-                                    <span class="text-white"><i class="fa fa-user mr-2"></i>{{$name[0]->instructor_name}}</span>
-                                    <span class="text-white"><i class="fa fa-star mr-2"></i>{{$course->course_rate}}
-                                        <small>({{rand(100,150)}})</small></span>
+                    <?php $name = DB::select('select instructor_name from instructors where id ='.$course->instructor_id);?>
+                    <div class="col-lg-4 col-md-6 pb-4">
+                        <a class="courses-list-item position-relative d-block overflow-hidden mb-2" href="{{ Route('course.show',$course->id ) }}">
+                            <img class="img-fluid" src="{{Storage::url($course->course_img)}}" onerror="this.src='{{ URL::to('/') }}/img/no-background.jpg';">
+                            <div class="courses-text">
+                                <h4 class="text-center text-white px-3">{{$course->course_name}}</h4>
+                                <div class="border-top w-100 mt-3">
+                                    <div class="d-flex justify-content-between p-4">
+                                        <span class="text-white"><i class="fa fa-user mr-2"></i>{{$name[0]->instructor_name}}</span>
+                                        <span class="text-white"><i class="fa fa-star mr-2"></i>{{$course->course_rate}}
+                                            <small>({{rand(100,150)}})</small></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </a>
-                </div>
-
+                        </a>
+                    </div>
                 @endforeach
+
+                {{-- {{$courses->links()}} --}}
 
                 <div class="col-12">
                     <nav aria-label="Page navigation">
                         <ul class="pagination pagination-lg justify-content-center mb-0">
-                        <li class="page-item disabled">
-                            <a class="page-link rounded-0" href="#" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                            <span class="sr-only">Previous</span>
-                            </a>
-                        </li>
-                        <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                            <a class="page-link rounded-0" href="#" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                            <span class="sr-only">Next</span>
-                            </a>
-                        </li>
+                            @php
+                                $count = $courses->count();
+                                $current = $courses->currentPage();
+                                $firstPageBool = $courses->onFirstPage();
+                                $currentPage = $courses->currentPage();
+                                $lastPage = $courses->lastPage();
+                            @endphp
+                            @if ($firstPageBool)
+                                <li class="page-item disabled">
+                                    <a class="page-link rounded-0" href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                    <span class="sr-only">Previous</span>
+                                    </a>
+                                </li>
+                            @else
+                                <li class="page-item">
+                                    <a class="page-link rounded-0" href="{{$courses->previousPageUrl()}}" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                    <span class="sr-only">Previous</span>
+                                    </a>
+                                </li>
+                            @endif
+
+                            @for ($i=1; $i != $lastPage +1; $i++)
+
+                                @if ($current==$i)
+                                    <li class="page-item active"><a class="page-link" href="">{{$i}}</a></li>
+                                @else
+                                    <li class="page-item"><a class="page-link" href="{{$courses->url($i)}}">{{$i}}</a></li>
+                                @endif
+
+                            @endfor
+
+                            @if ($i==$lastPage)
+                                <li class="page-item disabled">
+                                    <a class="page-link rounded-0" href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                    <span class="sr-only">Next</span>
+                                    </a>
+                                </li>
+                            @else
+                                <li class="page-item">
+                                    <a class="page-link rounded-0" href="{{$courses->nextPageUrl()}}" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                    <span class="sr-only">Next</span>
+                                    </a>
+                                </li>
+                            @endif
                         </ul>
                     </nav>
                 </div>
