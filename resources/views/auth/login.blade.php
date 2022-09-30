eeb3f721de3dfd2a85c7db988e602e462afdbea0
Khaled Eldesoukey <54212167+ImmortalBoi@users.noreply.github.com>
2022-09-30 12:50:02 +0200

    added login darkmode

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,25 +8,24 @@
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
 
-    <section class="vh-100" style="background-color: #9A616D;">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col col-xl-10">
-                    <div class="card" style="border-radius: 1rem;">
-                        <div class="row g-0">
-                            <div class="col-md-6 col-lg-5 d-none d-md-block">
+    <section class="bg-lightblue dark:bg-darkblue ">
+        <div class="dropshadow container h-full mx-auto sm:px-4 py-5 h-full flex flex-wrap flex justify-center items-center ">
+                <div class="relative flex-grow max-w-full flex-1 px-4 xl:w-4/5 pr-4 pl-4">
+                    <div class="relative flex flex-col min-w-0 rounded break-words bg-white drop-shadow-xl dark:bg-zinc-900 dark:text-white">
+                        <div class="flex items-center g-0">
+                            <div class="md:w-1/2 lg:w-2/5 hidden md:block">
                                 <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
-                                    alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                                    alt="login form" class="max-w-auto h-[95vh]" style="border-radius: 1rem 0 0 1rem;" />
                             </div>
-                            <div class="col-md-6 col-lg-7 d-flex align-items-center">
-                                <div class="card-body p-4 p-lg-5 text-black">
-                                    <div class="Back">
-                                        <button name="BackBtn" type="button" style="color: white;
-                                        padding: 8px 20px;
-                                        border: none;
-                                        border-radius: 4px;
-                                        cursor: pointer;
-                                        display: inline-block; font-size: 16px; float: right; border-radius: 8px; background-color: black;" onclick="history.back()">Back</button>
+                                <div class="flex-auto p-6 p-6 lg:p-12 text-black">
+                                    <div class="flex flex-row justify-between">
+                                        <div class="flex items-center mb-3 pb-1">
+                                            <i class="fa fa-book-reader fa-2x me-3" style="color:blue;"></i>
+                                            <span class="text-3xl ml-2 mb-0 dark:text-white">Course Forum</span>
+                                        </div>
+
+                                        <button name="BackBtn" type="button" class="text-gray-900 border border-gray-800 hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+                                        onclick="history.back()">Back</button>
                                     </div>
 
                                     <!-- Validation Errors -->
@@ -35,25 +34,20 @@
                                     <form method="POST" action="{{ route('login') }}">
                                         @csrf
 
-                                        <div class="d-flex align-items-center mb-3 pb-1">
-                                            <i class="fa fa-book-reader fa-2x me-3" style="color:blue;"></i>
-                                            <span class="h1 fw-bold mb-0">Course Forum</span>
-                                        </div>
-
-                                        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your
+                                        <h5 class="fw-normal mb-3 pb-3 dark:text-white" style="letter-spacing: 1px;">Sign into your
                                             account</h5>
 
                                         <div class="form-outline mb-4">
-                                            <x-text-input id="email" class="form-control form-control-lg"
+                                            <label class="mb-5 dark:text-white" for="form2Example17">Email address</label>
+                                            <x-text-input id="email" class="drop-shadow-xl bg-gray-50 border-2 border-gray-300 text-darkblue dark:text-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                 type="email" name="email" :value="old('email')" required autofocus />
-                                            <label class="form-label" for="form2Example17">Email address</label>
                                         </div>
 
                                         <div class="form-outline mb-4">
-                                            <x-text-input id="password" class="form-control form-control-lg"
+                                            <label class="dark:text-white" for="form2Example27">Password</label>
+                                            <x-text-input id="password" class="drop-shadow-xl bg-gray-50 border-2 border-gray-300 text-darkblue dark:text-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                 type="password" name="password" required
                                                 autocomplete="current-password" />
-                                            <label class="form-label" for="form2Example27">Password</label>
                                         </div>
 
                                         <div class="block mt-4 flex justify-between">
@@ -64,7 +58,7 @@
                                                 <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                                             </label>
                                             @if (Route::has('password.request'))
-                                                <a class="cursor-pointer tracking-tighter text-black border-b-2 border-gray-200 hover:border-gray-400"
+                                                <a class="dark:text-white cursor-pointer tracking-tighter text-black border-b-2 border-gray-200 hover:border-gray-400"
                                                     href="{{ route('password.request') }}">
                                                     {{ __('Forgot your password?') }}
                                                 </a>
@@ -72,23 +66,20 @@
                                         </div>
 
                                         <div class="pt-1 mb-4">
-                                            <button class="btn btn-dark btn-lg btn-block text-black" type="submit"
+                                            <button class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded  no-underline bg-gray-200 hover:text-white hover:bg-gray-900 py-3 px-4 leading-tight text-xl block w-full text-black" type="submit"
                                                 name="submit">Login</button>
                                         </div>
 
-                                        <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a
+                                        <p class="mb-5 lg:pb-2" style="color: #393f81;">Don't have an account? <a
                                                 href="{{ route('register') }}" style="color: #393f81;">Register here</a>
                                         </p>
-                                        <a href="#!" class="small text-muted">Terms of use.</a>
-                                        <a href="#!" class="small text-muted">Privacy policy</a>
+                                        <a href="#!" class="text-xs text-gray-700">Terms of use.</a>
+                                        <a href="#!" class="text-xs text-gray-700">Privacy policy</a>
                                     </form>
 
                                 </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
-            </div>
         </div>
     </section>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,13 +9,13 @@
 
     <section class="">
         <!-- Jumbotron -->
-        <div class="px-4 py-5 px-md-5 text-center text-lg-start" style="background-color: hsl(0, 0%, 96%)">
-            <div class="container">
-                <div class="row gx-lg-5 align-items-center">
-                    <div class="col-lg-6 mb-5 mb-lg-0">
-                        <h1 class="my-5 display-3 fw-bold ls-tight">
+        <div class="px-4 py-5 md:px-12 text-center text-lg-start" style="background-color: hsl(0, 0%, 96%)">
+            <div class="container mx-auto sm:px-4">
+                <div class="flex flex-wrap  gx-lg-5 items-center">
+                    <div class="lg:w-1/2 pr-4 pl-4 mb-5 lg:mb-0">
+                        <h1 class="my-5 text-3xl fw-bold ls-tight">
                             The best offer <br />
-                            <span class="text-primary">for your business</span>
+                            <span class="text-blue-600">for your business</span>
                         </h1>
                         <p style="color: hsl(217, 10%, 50.8%)">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -25,9 +25,9 @@
                         </p>
                     </div>
 
-                    <div class="col-lg-6 mb-5 mb-lg-0">
-                        <div class="card">
-                            <div class="card-body py-5 px-md-5">
+                    <div class="lg:w-1/2 pr-4 pl-4 mb-5 lg:mb-0">
+                        <div class="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300">
+                            <div class="flex-auto p-6 py-5 md:px-12">
                                 <div class="Back">
                                     <button name="BackBtn" type="button" style="color: white;
                                     padding: 8px 20px;
@@ -42,10 +42,10 @@
                                 <form method="POST" action="{{ route('register') }}">
                                     @csrf
                                     <!-- 2 column grid layout with text inputs for the first and last names -->
-                                    <div class="row">
-                                        <div class="col-md-12 mb-4">
+                                    <div class="flex flex-wrap ">
+                                        <div class="md:w-full pr-4 pl-4 mb-4">
                                             <div class="form-outline">
-                                                <x-text-input id="name" class="form-control" type="text"
+                                                <x-text-input id="name" class="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded" type="text"
                                                     name="name" :value="old('name')" required autofocus />
                                                 <label class="form-label" for="form3Example1">Username</label>
                                             </div>
@@ -55,53 +55,53 @@
 
                                     <!-- Email input -->
                                     <div class="form-outline mb-4">
-                                        <x-text-input id="email" class="form-control" type="email" name="email"
+                                        <x-text-input id="email" class="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded" type="email" name="email"
                                             :value="old('email')" required />
                                         <label class="form-label" for="form3Example3">Email address</label>
                                     </div>
 
                                     <!-- Password input -->
                                     <div class="form-outline mb-4">
-                                        <x-text-input id="password" class="form-control" type="password"
+                                        <x-text-input id="password" class="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded" type="password"
                                             name="password" required autocomplete="new-password" />
                                         <label class="form-label" for="form3Example4">Password</label>
                                     </div>
 
                                     <!--Confirm Password input -->
                                     <div class="form-outline mb-4">
-                                        <x-text-input id="password_confirmation" class="form-control" type="password"
+                                        <x-text-input id="password_confirmation" class="block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded" type="password"
                                             name="password_confirmation" required />
                                         <label class="form-label" for="form3Example4">Confirm Password</label>
                                     </div>
 
                                     <!--Already Registered -->
-                                    <div class="form-check d-flex justify-content-center mb-4">
-                                        <a class="btn btn-link mx-1" href="{{ route('login') }}">
+                                    <div class="relative block mb-2 flex justify-center mb-4">
+                                        <a class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline font-normal text-blue-700 bg-transparent mx-1" href="{{ route('login') }}">
                                             {{ __('Already registered?') }}
                                         </a>
                                     </div>
 
                                     <!-- Submit button -->
-                                    <button type="submit" name="submit" class="btn btn-primary btn-block mb-4">
+                                    <button type="submit" name="submit" class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600 block w-full mb-4">
                                         Sign up
                                     </button>
 
                                     <!-- Register buttons -->
                                     <div class="text-center">
                                         <p>or sign up with:</p>
-                                        <button type="button" class="btn btn-link btn-floating mx-1">
+                                        <button type="button" class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline font-normal text-blue-700 bg-transparent btn-floating mx-1">
                                             <i class="fab fa-facebook-f"></i>
                                         </button>
 
-                                        <button type="button" class="btn btn-link btn-floating mx-1">
+                                        <button type="button" class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline font-normal text-blue-700 bg-transparent btn-floating mx-1">
                                             <i class="fab fa-google"></i>
                                         </button>
 
-                                        <button type="button" class="btn btn-link btn-floating mx-1">
+                                        <button type="button" class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline font-normal text-blue-700 bg-transparent btn-floating mx-1">
                                             <i class="fab fa-twitter"></i>
                                         </button>
 
-                                        <button type="button" class="btn btn-link btn-floating mx-1">
+                                        <button type="button" class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline font-normal text-blue-700 bg-transparent btn-floating mx-1">
                                             <i class="fab fa-github"></i>
                                         </button>
 
