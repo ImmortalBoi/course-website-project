534f86bf57bf3a1d32d83985c30ae92a7bdbf969
Ahmed Hemida <ahmedhemida121@gmail.com>
2022-10-01 16:31:56 +0200

    Magic Trick With Title of The Students pages

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -26,7 +26,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>@yield('frontTitle')</title>
 
         <!-- Google Web Fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
