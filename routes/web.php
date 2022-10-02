cbd4767116f6db3b5e6c195aa252f97c1afb0d59
Ahmed Hemida <ahmedhemida121@gmail.com>
2022-10-02 11:37:09 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,21 +26,19 @@ Route::get('/about', function () {
 Route::resource('/contact', ContactController::class);
 Route::get('/contact/messages', [ContactController::class,'messages'])->name('messages');
 
-
-Route::get('/course', [FrontCourseController::class, 'index'])->name('course.index');
-Route::get('/course/{id}', [FrontCourseController::class, 'show'])->name('course.show');
-
-
 Route::get('/team', function () {
     return view('team');
 });
 
-Route::get('/profile',[ProfileController::class,'index'])->name('profile');
-Route::get('/profile/edit/{id}',[ProfileController::class,'edit'])->name('editProflie');
-Route::put('/profile/update/{id}',[ProfileController::class,'update'])->name('updateProfile');
+Route::controller(FrontCourseController::class)->group(function(){
+    Route::get('/course', 'index')->name('course.index');
+    Route::get('/course/{id}', 'show')->name('course.show');
+});
 
-Route::get('/profile/edit', function () {
-    return view('student.edit');
+Route::controller(ProfileController::class)->group(function () {
+    Route::get('/profile', 'index')->name('profile');
+    Route::get('/profile/edit/{id}','edit')->name('editProflie');
+    Route::put('/profile/update/{id}','update')->name('updateProfile');
 });
 
 Route::get('/dashboard', function () {
@@ -48,7 +46,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth','admin'])->name('dashboard');
 
 Route::middleware(['auth','admin'])->name('admin.')->prefix('admin')->group(function(){
-
     Route::get('/',[AdminController::class,'index'])->name('index');
     Route::resource('/courses', CourseController::class);
     Route::resource('/instructors', InstructorController::class);
